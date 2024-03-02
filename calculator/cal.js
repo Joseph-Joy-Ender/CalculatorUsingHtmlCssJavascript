c202e65e98d4137c2e788019d499941352b83be8
Joseph-Joy-Ender <144816729+Joseph-Joy-Ender@users.noreply.github.com>
2024-03-02 23:49:13 +0100

    "calculator app"

---
+++ calculator/cal.js
@@ -0,0 +1,26 @@
+const display = document.querySelector('.display');
+const input = document.querySelectorAll("input");
+const operators = ['%', '*', '/', '+', '=', '-'];
+let output = "";
+
+
+const calculation = (btnValue) =>{
+    if (btnValue === "=" && output !== ""){
+        output = eval(output.replace("%", "/100"));
+    }else if (btnValue === "DEL"){
+        output = output.toString().slice(0, -1);
+    }else if (btnValue === "AC"){
+        output = "";
+    }else {
+        if (output === "" && operators.includes(btnValue)) return;
+        output += btnValue;
+    }
+    display.value = output;
+};
+
+
+input.forEach((button) =>{
+    button.addEventListener("click", (e) => calculation(e.target.dataset.value));
+});
+
+
